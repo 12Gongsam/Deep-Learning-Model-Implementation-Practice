65b3b4169cf4ca590b29076e58677adff10e0ec5
12Gongsam <juunaaa1203@gmail.com>
2025-03-07 12:12:25 +0900

    250307

--- level_1/lenet5.py
+++ level_1/lenet5.py
@@ -1,44 +1,42 @@
 import torch
 import torch.nn as nn
-
+from torchinfo import summary
 
 class LeNet5(nn.Module):
     def __init__(self, n_classes=10):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.Tanh(),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.Tanh(),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5),
-            nn.Tanh(),
-            nn.Flatten(),
-            nn.Linear(in_features=120, out_features=84),
-            nn.Tanh(),
-            nn.Linear(in_features=84, out_features=n_classes),
-            nn.Softmax(dim=1), # instead of RBF
-        )
+        super(LeNet5, self).__init__()
         
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
+        
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+        
+        self.fc1 = nn.Linear(in_features=120, out_features=84)
+        self.fc2 = nn.Linear(in_features=84, out_features=n_classes)
+
+        self.activation = nn.Tanh()
 
     def forward(self, x):
-        out = self.layers(x)
-        return out
+        x = self.activation(self.conv1(x))
+        x = self.pool(x)
+
+        x = self.activation(self.conv2(x))
+        x = self.pool(x)
+
+        x = self.activation(self.conv3(x))
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.activation(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
     
 
 if __name__ == "__main__":
     model = LeNet5()
     input = torch.randn(size=(1, 1, 32, 32))
-    output = model(input)
-    print("Output shape:", output.shape)
     
     # 모델 구조 출력
     print("\nModel structure:")
-    print(model)
-    
-    # 전체 파라미터 수 계산
-    total_params = sum(p.numel() for p in model.parameters()) # numel은 텐서의 총 원소 개수를 반환하는 메서드
-    print("\nTotal number of parameters:", total_params)
+    summary(model, input_data=input)
